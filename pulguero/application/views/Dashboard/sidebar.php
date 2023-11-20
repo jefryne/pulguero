822d9ed4db86e65eb26567f3023f3bc48d7ec5ca
jefryne <jefryne89@gmail.com>
2023-11-20 13:56:30 -0500

    nav arreglado

--- pulguero/application/views/Dashboard/sidebar.php
+++ pulguero/application/views/Dashboard/sidebar.php
@@ -65,43 +65,29 @@
             </a>
           </li>
           <li class="nav-item menu-items">
-            <a class="nav-link"  href="http://[::1]/pulguero/index.php/Usuarios/listadoUsuarios">
-              <span class="menu-icon">
-                <i class="mdi mdi-laptop"></i>
-              </span>
-              <span class="menu-title">Lista usuarios</span>
-              <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="ui-basic">
-              <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="../../pages/ui-features/buttons.html">Buttons</a></li>
-                <li class="nav-item"> <a class="nav-link" href="../../pages/ui-features/dropdowns.html">Dropdowns</a></li>
-                <li class="nav-item"> <a class="nav-link" href="../../pages/ui-features/typography.html">Typography</a></li>
-              </ul>
-            </div>
-          </li>
-          <li class="nav-item menu-items">
-            <a class="nav-link" href="http://[::1]/pulguero/index.php/Usuarios/register">
-              <span class="menu-icon">
-                <i class="mdi mdi-playlist-play"></i>
-              </span>
-              <span class="menu-title">Registrar</span>
-            </a>
-          </li>
-          <li class="nav-item menu-items">
-            <a class="nav-link" href="http://[::1]/pulguero/index.php/Usuarios/crear_cuenta">
+            <a class="nav-link" data-toggle="collapse" href="#usuarios_nav" aria-expanded="false" aria-controls="usuarios_nav">
               <span class="menu-icon">
                 <i class="mdi mdi-table-large"></i>
               </span>
-              <span class="menu-title">Crear cuenta</span>
+              <span class="menu-title">Usuarios</span>
+              <i class="menu-arrow"></i>
             </a>
+            <div class="collapse" id="usuarios_nav">
+              <ul class="nav flex-column sub-menu">
+              <li class="nav-item"> <a class="nav-link" href="http://[::1]/pulguero/index.php/Usuarios/register"> Registrar</a></li>
+                <li class="nav-item"> <a class="nav-link" href="http://[::1]/pulguero/index.php/Usuarios/listadoUsuarios">Lista usuarios</a></li>
+                <li class="nav-item"> <a class="nav-link" href="http://[::1]/pulguero/index.php/Usuarios/crear_cuenta"> Crear cuenta </a></li>
+              </ul>
+            </div>
           </li>
+
           <li class="nav-item menu-items">
-            <a class="nav-link" href="http://[::1]/pulguero/index.php/Inventarios/inventario">
+            <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
               <span class="menu-icon">
                 <i class="mdi mdi-table-large"></i>
               </span>
               <span class="menu-title">Inventario</span>
+              <i class="menu-arrow"></i>
             </a>
             <div class="collapse" id="auth">
               <ul class="nav flex-column sub-menu">
@@ -109,51 +95,8 @@
                 <li class="nav-item"> <a class="nav-link" href="http://[::1]/pulguero/index.php/Inventarios/listadoInventario"> Listado Inventario </a></li>
                 <li class="nav-item"> <a class="nav-link" href="../../pages/samples/error-500.html"> 500 </a></li>
                 <li class="nav-item"> <a class="nav-link" href="../../pages/samples/login.html"> Login </a></li>
-                <li class="nav-item"> <a class="nav-link" href="../../pages/samples/register.html"> Register </a></li>
               </ul>
             </div>
-          </li>
-          <li class="nav-item menu-items">
-            <a class="nav-link" href="../../pages/charts/chartjs.html">
-              <span class="menu-icon">
-                <i class="mdi mdi-chart-bar"></i>
-              </span>
-              <span class="menu-title">Charts</span>
-            </a>
-          </li>
-          <li class="nav-item menu-items">
-            <a class="nav-link" href="../../pages/icons/mdi.html">
-              <span class="menu-icon">
-                <i class="mdi mdi-contacts"></i>
-              </span>
-              <span class="menu-title">Icons</span>
-            </a>
-          </li>
-          <li class="nav-item menu-items">
-            <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-              <span class="menu-icon">
-                <i class="mdi mdi-security"></i>
-              </span>
-              <span class="menu-title">User Pages</span>
-              <i class="menu-arrow"></i>
-            </a>
-            <!-- <div class="collapse" id="auth">
-              <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="../../pages/samples/blank-page.html"> Blank Page </a></li>
-                <li class="nav-item"> <a class="nav-link" href="../../pages/samples/error-404.html"> 404 </a></li>
-                <li class="nav-item"> <a class="nav-link" href="../../pages/samples/error-500.html"> 500 </a></li>
-                <li class="nav-item"> <a class="nav-link" href="../../pages/samples/login.html"> Login </a></li>
-                <li class="nav-item"> <a class="nav-link" href="../../pages/samples/register.html"> Register </a></li>
-              </ul>
-            </div> -->
-          </li>
-          <li class="nav-item menu-items">
-            <a class="nav-link" href="http://www.bootstrapdash.com/demo/corona-free/jquery/documentation/documentation.html">
-              <span class="menu-icon">
-                <i class="mdi mdi-file-document-box"></i>
-              </span>
-              <span class="menu-title">Documentation</span>
-            </a>
           </li>
         </ul>
       </nav>
