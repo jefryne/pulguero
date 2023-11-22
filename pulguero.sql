fef04e2213ebba57298ad57e32a21632acfba4af
jefryne <jefryne89@gmail.com>
2023-11-22 14:52:30 -0500

    db factura

--- pulguero.sql
+++ pulguero.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-11-2023 a las 16:50:42
--- Versión del servidor: 10.4.27-MariaDB
--- Versión de PHP: 8.2.0
+-- Tiempo de generación: 22-11-2023 a las 20:50:41
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -38,9 +38,7 @@ CREATE TABLE `account` (
 --
 
 INSERT INTO `account` (`id_account`, `id_user`, `password`) VALUES
-(2, 6, '$2y$10$2PogFAa/NgCqwjmDOjRtPOPCERVhsgiVBZf1NQ49Yd0nZb0JCOl.u'),
-(3, 1, '$2y$10$u6o26CeyFgWuycFkg0dhsOF7anfXAzzwOGQ6rM.b0oQCdLU7ktEF6'),
-(4, 18, '$2y$10$ynCrVpy6IcFkiJTx4WpIUuS6N3AWa0UAnOuxY9bhRLGKL8Dmt07Ne');
+(1, 1, '$2y$10$u6o26CeyFgWuycFkg0dhsOF7anfXAzzwOGQ6rM.b0oQCdLU7ktEF6');
 
 -- --------------------------------------------------------
 
@@ -54,14 +52,6 @@ CREATE TABLE `accumulated` (
   `quantity` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Volcado de datos para la tabla `accumulated`
---
-
-INSERT INTO `accumulated` (`id_accumulated`, `id_user`, `quantity`) VALUES
-(1, 15, 0),
-(2, 16, 80);
-
 -- --------------------------------------------------------
 
 --
@@ -72,13 +62,6 @@ CREATE TABLE `category` (
   `id_category` int(10) UNSIGNED NOT NULL,
   `category_name` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `category`
---
-
-INSERT INTO `category` (`id_category`, `category_name`) VALUES
-(1, 'dulces');
 
 -- --------------------------------------------------------
 
@@ -91,19 +74,8 @@ CREATE TABLE `history` (
   `id_inventory` int(10) UNSIGNED NOT NULL,
   `id_user` int(10) UNSIGNED NOT NULL,
   `id_client` int(10) UNSIGNED NOT NULL,
-  `date_history` timestamp NOT NULL DEFAULT current_timestamp(),
-  `transaction_status` enum('Venta') NOT NULL
+  `date_history` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `history`
---
-
-INSERT INTO `history` (`id_history`, `id_inventory`, `id_user`, `id_client`, `date_history`, `transaction_status`) VALUES
-(1, 1, 1, 2, '2023-11-19 05:51:33', 'Venta'),
-(2, 1, 1, 16, '2023-11-21 13:53:56', 'Venta'),
-(9, 1, 18, 16, '2023-11-21 15:41:50', 'Venta'),
-(10, 1, 18, 15, '2023-11-21 15:45:55', 'Venta');
 
 -- --------------------------------------------------------
 
@@ -113,8 +85,6 @@ INSERT INTO `history` (`id_history`, `id_inventory`, `id_user`, `id_client`, `da
 
 CREATE TABLE `inventory` (
   `id_inventory` int(10) UNSIGNED NOT NULL,
-  `nombre` varchar(150) NOT NULL,
-  `descripcion` text NOT NULL,
   `id_user` int(10) UNSIGNED NOT NULL,
   `id_category` int(10) UNSIGNED NOT NULL,
   `cost` int(10) UNSIGNED NOT NULL,
@@ -122,12 +92,29 @@ CREATE TABLE `inventory` (
   `status_inventory` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
 --
--- Volcado de datos para la tabla `inventory`
+-- Estructura de tabla para la tabla `invoice`
 --
 
-INSERT INTO `inventory` (`id_inventory`, `nombre`, `descripcion`, `id_user`, `id_category`, `cost`, `price`, `status_inventory`) VALUES
-(1, 'caramelo', 'de chocolate', 2, 1, 100, 200, 1);
+CREATE TABLE `invoice` (
+  `id_invoice` int(10) UNSIGNED NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `total` float(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `item_invoice`
+--
+
+CREATE TABLE `item_invoice` (
+  `id_item` int(10) UNSIGNED NOT NULL,
+  `id_invoice` int(10) UNSIGNED DEFAULT NULL,
+  `id_inventory` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -164,13 +151,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id_user`, `document_number`, `user_name`, `user_last_name`, `email`, `rol`, `status_user`, `cellphone`) VALUES
-(1, '777', 'fefe', 'nunez', 'fefe@gmail.com', 'Admin', 1, '44445'),
-(2, '100', 'pepe', 'dasd', 'sad', 'Cliente', 1, 'asd'),
-(6, '000', 'luna', '', 'luna@gmail.com', 'Admin', 1, ''),
-(15, '333', 'luna', 'perez', 'luna4454@gmail.com', 'Vendedor', 1, '8888'),
-(16, '0101', 'pepi', 'toto', 'pepi@gmail.com', 'Vendedor', 1, '8888'),
-(17, '8888', 'supervisor', 'visor', 'popo@gmail.com', 'SuperVisor', 1, '123'),
-(18, '987', 'monica', 'acosta', 'moni@gmail.com', 'SuperVisor', 1, '313560');
+(1, '100800', 'fefe', 'lopez', 'fefe@gmail.com', 'Admin', 1, '7777');
 
 --
 -- Índices para tablas volcadas
@@ -214,6 +195,20 @@ ALTER TABLE `inventory`
   ADD KEY `id_category` (`id_category`);
 
 --
+-- Indices de la tabla `invoice`
+--
+ALTER TABLE `invoice`
+  ADD PRIMARY KEY (`id_invoice`);
+
+--
+-- Indices de la tabla `item_invoice`
+--
+ALTER TABLE `item_invoice`
+  ADD PRIMARY KEY (`id_item`),
+  ADD KEY `id_invoice` (`id_invoice`),
+  ADD KEY `id_inventory` (`id_inventory`);
+
+--
 -- Indices de la tabla `liquidation`
 --
 ALTER TABLE `liquidation`
@@ -235,31 +230,43 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `account`
 --
 ALTER TABLE `account`
-  MODIFY `id_account` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_account` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `accumulated`
 --
 ALTER TABLE `accumulated`
-  MODIFY `id_accumulated` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_accumulated` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `category`
 --
 ALTER TABLE `category`
-  MODIFY `id_category` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_category` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `history`
 --
 ALTER TABLE `history`
-  MODIFY `id_history` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_history` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `inventory`
 --
 ALTER TABLE `inventory`
-  MODIFY `id_inventory` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_inventory` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `invoice`
+--
+ALTER TABLE `invoice`
+  MODIFY `id_invoice` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `item_invoice`
+--
+ALTER TABLE `item_invoice`
+  MODIFY `id_item` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `liquidation`
@@ -271,7 +278,7 @@ ALTER TABLE `liquidation`
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id_user` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restricciones para tablas volcadas
@@ -303,6 +310,13 @@ ALTER TABLE `history`
 ALTER TABLE `inventory`
   ADD CONSTRAINT `inventory_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`),
   ADD CONSTRAINT `inventory_ibfk_2` FOREIGN KEY (`id_category`) REFERENCES `category` (`id_category`);
+
+--
+-- Filtros para la tabla `item_invoice`
+--
+ALTER TABLE `item_invoice`
+  ADD CONSTRAINT `item_invoice_ibfk_1` FOREIGN KEY (`id_invoice`) REFERENCES `invoice` (`id_invoice`),
+  ADD CONSTRAINT `item_invoice_ibfk_2` FOREIGN KEY (`id_inventory`) REFERENCES `inventory` (`id_inventory`);
 
 --
 -- Filtros para la tabla `liquidation`
